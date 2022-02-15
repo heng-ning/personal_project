c0cf815200fb26c34656f02c491d9eb32089f386
heng-ning <maggie310254@gmail.com>
2022-02-15 14:26:31 +0800

    l;

--- index.js
+++ index.js
@@ -10,7 +10,19 @@
 // });
 
 
-
+//arrow
+// $(function(){
+//     showScroll();
+//     function showScroll(){
+//     $(window).scroll( function() {
+//     var scrollValue=$(window).scrollTop();
+//     scrollValue > 100 ? $('div[class=scroll]').fadeIn():$('div[class=scroll]').fadeOut();
+//     } );
+//     $('#scroll').click(function(){
+//     $("html,body").animate({scrollTop:0},200);
+//     });
+//     }
+//     })
 
 
 
@@ -21,8 +33,11 @@
 // nav
 
 
-
-
+//menu
+// var detail = document.getElementsByClassName('detail')[0];
+// detail.addEventListener("click",finction(e){
+    
+// });
 // question
 
 // function Show_Hidden(obj)
